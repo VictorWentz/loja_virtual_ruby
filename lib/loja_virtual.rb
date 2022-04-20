112cff384837af2f3f93428ac01af0aecbe2d9c7
VictorWentz <victorhwentz@gmail.com>
2022-04-20 16:32:58 -0300

    cap6

--- lib/loja_virtual.rb
+++ lib/loja_virtual.rb
@@ -1,5 +1,6 @@
 require File.expand_path('lib\book.rb')
 require File.expand_path('lib\biblioteca.rb')
 require File.expand_path('lib\relatorio.rb')
+require File.expand_path('lib\banco_de_arquivos.rb')
 
 
